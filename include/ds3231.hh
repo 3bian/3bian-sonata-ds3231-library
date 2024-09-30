46665f090456065797a0e2615a56ce48159b1489
adam-3bian <adam@3bian.co.uk>
2024-09-30 13:07:21 +0100

    Add ability for control register and datetime to be set

--- include/ds3231.hh
+++ include/ds3231.hh
@@ -9,18 +9,21 @@
 namespace DS3231
 {
     /// @brief I2C address for the DS3231 RTC module.
-    constexpr uint8_t I2C_ADDRESS = 0x68;
+    constexpr uint8_t I2C_ADDRESS      = 0x68;
+
+    /// @brief Register address for the ControlRegister in the DS3231.
+    constexpr uint8_t REG_CONTROL      = 0x0E;
 
     /// @brief Register address for DateTime in the DS3231.
-    constexpr uint8_t REG_DATETIME = 0x00;
+    constexpr uint8_t REG_DATETIME     = 0x00;
 
     /// @brief Register address for Temperature in the DS3231.
-    constexpr uint8_t REG_TEMPERATURE = 0x11;
+    constexpr uint8_t REG_TEMPERATURE  = 0x11;
 
     /// @brief Enum class representing days of the week for the DS3231.
     enum class Weekday : uint8_t 
     {
-        Sunday = 0,
+        Sunday    = 0,
         Monday,
         Tuesday,
         Wednesday,
@@ -31,30 +34,43 @@ namespace DS3231
 
     #pragma pack(push, 1)
 
+    /// @brief Structure representing control data stored in the DS3231.
+    struct Control
+    {
+        bool           aral         : 1;    // Alarm 1 Interrupt Enable (bit 0)
+        bool           a2ie         : 1;    // Alarm 2 Interrupt Enable (bit 1)
+        bool           intcn        : 1;    // Interrupt Control (bit 2)
+        bool           rs1          : 1;    // Rate Select 1 (bit 3)
+        bool           rs2          : 1;    // Rate Select 2 (bit 4)
+        bool           conv         : 1;    // Convert Temperature (bit 5)
+        bool           bbsqw        : 1;    // Battery-Backed Square-Wave Enable (bit 6)
+        bool           eosc         : 1;    // Enable Oscillator (bit 7)
+    };
+
     /// @brief Structure representing Date and Time stored in the DS3231 registers.
     struct DateTime
     {
-        uint8_t       second_units  : 4;
-        uint8_t       second_tens   : 3;
+        uint8_t        second_units : 4;
+        uint8_t        second_tens  : 3;
         uint8_t                     : 1;
-        uint8_t       minute_units  : 4;
-        uint8_t       minute_tens   : 3;
+        uint8_t        minute_units : 4;
+        uint8_t        minute_tens  : 3;
         uint8_t                     : 1;
-        uint8_t       hour_units    : 4;
-        uint8_t       hour_tens     : 2;
-        bool          is_24_hour    : 1;
+        uint8_t        hour_units   : 4;
+        uint8_t        hour_tens    : 2;
+        bool           is_24_hour   : 1;
         uint8_t                     : 1;
-        Weekday       weekday       : 3;
+        Weekday        weekday      : 3;
         uint8_t                     : 5;
-        uint8_t       day_units     : 4;
-        uint8_t       day_tens      : 2;
+        uint8_t        day_units    : 4;
+        uint8_t        day_tens     : 2;
         uint8_t                     : 2;
-        uint8_t       month_units   : 4;
-        uint8_t       month_tens    : 1;
+        uint8_t        month_units  : 4;
+        uint8_t        month_tens   : 1;
         uint8_t                     : 2;
-        uint8_t       century       : 1;
-        uint8_t       year_units    : 4;
-        uint8_t       year_tens     : 4;
+        uint8_t        century      : 1;
+        uint8_t        year_units   : 4;
+        uint8_t        year_tens    : 4;
     };
 
     /// @brief Structure representing Temperature data stored in the DS3231.
@@ -65,7 +81,27 @@ namespace DS3231
         uint8_t                     : 6;
     };
 
+    /// @brief Structure representing a register address with an associated payload.
+    /// @tparam T The type of the payload stored in the structure.
+    template <typename T>
+    struct RegisterPayload
+    {
+        uint8_t        reg;
+        T              payload;
+
+        RegisterPayload(uint8_t reg, const T& payload)
+            : reg(reg), payload(payload)
+        {
+        }
+    };
+
     #pragma pack(pop)
+
+    /// @brief Reads the Control register from the DS3231 RTC module.
+    /// @param i2c Pointer to an OpenTitanI2c instance used for communication.
+    /// @param control Pointer to a Control structure to store the read data.
+    /// @return true if the read operation is successful, false otherwise.
+    bool read_control(volatile OpenTitanI2c* i2c, Control* control);
 
     /// @brief Reads the DateTime from the DS3231 RTC module.
     /// @param i2c Pointer to an OpenTitanI2c instance used for communication.
@@ -78,4 +114,14 @@ namespace DS3231
     /// @param temperature Pointer to a Temperature structure to store the read data.
     /// @return true if the read operation is successful, false otherwise.
     bool read_temperature(volatile OpenTitanI2c* i2c, Temperature* temperature);
+
+    /// @brief Writes the Control register to the DS3231 RTC module.
+    /// @param i2c Pointer to an OpenTitanI2c instance used for communication.
+    /// @param control Pointer to a Control structure containing the data to write.
+    void write_control(volatile OpenTitanI2c* i2c, const Control* control);
+
+    /// @brief Writes the DateTime to the DS3231 RTC module.
+    /// @param i2c Pointer to an OpenTitanI2c instance used for communication.
+    /// @param datetime Pointer to a DateTime structure containing the data to write.
+    void write_datetime(volatile OpenTitanI2c* i2c, const DateTime* datetime);
 }
