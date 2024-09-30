f01e21e215c68d62c15a971fcdf1af1090de3320
adam-3bian <adam@3bian.co.uk>
2024-09-30 18:05:12 +0100

    Improve code style

--- src/ds3231.cc
+++ src/ds3231.cc
@@ -9,39 +9,39 @@ namespace DS3231
     {
         i2c->blocking_write(I2CAddress, &ControlRegisterAddress, sizeof(ControlRegisterAddress), true);
 
-        auto* read_buffer = reinterpret_cast<uint8_t*>(&control);
-        return i2c->blocking_read(I2CAddress, read_buffer, sizeof(Control));
+        auto* readBuffer = reinterpret_cast<uint8_t*>(&control);
+        return i2c->blocking_read(I2CAddress, readBuffer, sizeof(Control));
     }
 
     bool read_datetime(volatile OpenTitanI2c* i2c, DateTime& datetime)
     {
         i2c->blocking_write(I2CAddress, &DateTimeRegisterAddress, sizeof(DateTimeRegisterAddress), true);
 
-        auto* read_buffer = reinterpret_cast<uint8_t*>(&datetime);
-        return i2c->blocking_read(I2CAddress, read_buffer, sizeof(DateTime));
+        auto* readBuffer = reinterpret_cast<uint8_t*>(&datetime);
+        return i2c->blocking_read(I2CAddress, readBuffer, sizeof(DateTime));
     }
 
     bool read_temperature(volatile OpenTitanI2c* i2c, Temperature& temperature)
     {
         i2c->blocking_write(I2CAddress, &TemperatureRegisterAddress, sizeof(TemperatureRegisterAddress), true);
 
-        auto* read_buffer = reinterpret_cast<uint8_t*>(&temperature);
-        return i2c->blocking_read(I2CAddress, read_buffer, sizeof(Temperature));
+        auto* readBuffer = reinterpret_cast<uint8_t*>(&temperature);
+        return i2c->blocking_read(I2CAddress, readBuffer, sizeof(Temperature));
     }
 
     void write_control(volatile OpenTitanI2c* i2c, const Control& control)
     {
         RegisterPayload<Control> payload(ControlRegisterAddress, control);
 
-        auto* write_buffer = reinterpret_cast<uint8_t*>(&payload);
-        i2c->blocking_write(I2CAddress, write_buffer, sizeof(RegisterPayload<Control>), true);
+        auto* writeBuffer = reinterpret_cast<uint8_t*>(&payload);
+        i2c->blocking_write(I2CAddress, writeBuffer, sizeof(RegisterPayload<Control>), true);
     }
 
     void write_datetime(volatile OpenTitanI2c* i2c, const DateTime& datetime)
     {
         RegisterPayload<DateTime> payload(DateTimeRegisterAddress, datetime);
 
-        auto* write_buffer = reinterpret_cast<uint8_t*>(&payload);
-        i2c->blocking_write(I2CAddress, write_buffer, sizeof(RegisterPayload<DateTime>), true);
+        auto* writeBuffer = reinterpret_cast<uint8_t*>(&payload);
+        i2c->blocking_write(I2CAddress, writeBuffer, sizeof(RegisterPayload<DateTime>), true);
     }
 }
