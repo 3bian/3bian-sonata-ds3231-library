07e6bffe81a69dcf6972a17108c16cc568e4ea75
adam-3bian <adam@3bian.co.uk>
2024-10-04 12:09:58 +0100

    Modify ds3231-temperature.hh to use updated concept and bit functions

--- include/ds3231-temperature.hh
+++ include/ds3231-temperature.hh
@@ -1,52 +1,38 @@
-// Copyright 3bian Limited and CHERIoT Contributors.
-// SPDX-License-Identifier: Apache-2.0
-
 #pragma once
 
-#include <ds3231-common.hh>
+#include "common.hh"
 
 namespace DS3231
 {
-	template <I2c T>
+	template <I2cConcept T>
 	class Temperature
 	{
 	public:
-		Temperature(volatile T* i2c);
+		Temperature(T* i2c)
+			: i2c(i2c), registers{}
+		{
+		}
 
-		int8_t degrees();
-		uint8_t quarters();
+		int8_t temperature_whole()
+		{
+			return static_cast<int8_t>(extract_bits<0, 8>(registers[0]));
+		}
 
-		bool retrieve();
+		uint8_t temperature_quarters()
+		{
+			return extract_bits<6, 2>(registers[1]);
+		}
+
+		bool retrieve()
+		{
+			const uint8_t address = 0x11;
+
+			i2c->blocking_write(0x68, &address, sizeof(address), true);
+			return i2c->blocking_read(0x68, registers, sizeof(registers));
+		}
 
 	private:
-		volatile T* i2c;
+		T* i2c;
 		uint8_t registers[2];
 	};
-
-	template <I2c T>
-	Temperature<T>::Temperature(volatile T* i2c)
-		: i2c(i2c), registers{}
-	{
-	}
-
-	template <I2c T>
-	int8_t Temperature<T>::degrees()
-	{
-		return static_cast<int8_t>(extract_bits(registers, sizeof(registers), 0x00, 0x0, 0x8));
-	}
-
-	template <I2c T>
-	uint8_t Temperature<T>::quarters()
-	{
-		return extract_bits(registers, sizeof(registers), 0x01, 0x6, 0x2);
-	}
-
-	template <I2c T>
-	bool Temperature<T>::retrieve()
-	{
-		static const uint8_t address = 0x11;
-
-		i2c->blocking_write(0x68, &address, sizeof(address), true);
-		return i2c->blocking_read(0x68, registers, sizeof(registers));
-	}
 }
