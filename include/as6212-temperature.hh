65dcea52989eb9c2336f0a1818dca2cb8d22670c
adam-3bian <adam@3bian.co.uk>
2024-10-04 15:41:57 +0100

    Add AS6212 temperature sensor

---
+++ include/as6212-temperature.hh
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "common.hh"
+
+namespace AS6212
+{
+	template <I2cConcept T>
+	class Temperature
+	{
+	public:
+		Temperature(T* i2c)
+			: i2c(i2c), registers{}
+		{
+		}
+
+		int16_t whole()
+		{
+			uint8_t msb = registers[0];
+			uint8_t lsb = extract_bits<7, 1>(registers[1]);
+			return (msb << 1) | lsb;
+		}
+
+		uint8_t fraction()
+		{
+			return extract_bits<0, 7>(registers[1]);
+		}
+
+		bool retrieve()
+		{
+			const uint8_t address = 0x00;
+			i2c->blocking_write(0x48, &address, sizeof(address), true);
+			return i2c->blocking_read(0x48, registers, sizeof(registers));
+		}
+
+	private:
+		T* i2c;
+		uint8_t registers[2];
+	};
+}
