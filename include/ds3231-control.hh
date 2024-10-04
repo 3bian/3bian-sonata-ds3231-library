8a3a1caaa8d5166cfc6bd6caa705b26aa8c74e93
adam-3bian <adam@3bian.co.uk>
2024-10-04 11:48:29 +0100

    Modify ds3231-control.hh to use updated concept and bit functions

--- include/ds3231-control.hh
+++ include/ds3231-control.hh
@@ -1,169 +1,116 @@
-// Copyright 3bian Limited and CHERIoT Contributors.
-// SPDX-License-Identifier: Apache-2.0
-
 #pragma once
 
-#include <ds3231-common.hh>
+#include "common.hh"
 
 namespace DS3231
 {
-	template <I2c T>
+	template <I2cConcept T>
 	class Control
 	{
 	public:
-		Control(volatile T* i2c);
+		Control(T* i2c)
+			: i2c(i2c), registers{}
+		{
+		}
 
-		bool alarm1_interrupt_enable();
-		void set_alarm1_interrupt_enable(bool enable);
+		bool alarm1_interrupt_enable()
+		{
+			return extract_bits<0, 1>(registers[0]);
+		}
 
-		bool alarm2_interrupt_enable();
-		void set_alarm2_interrupt_enable(bool enable);
+		void set_alarm1_interrupt_enable(bool value)
+		{
+			assign_bits<0, 1>(&registers[0], value);
+		}
 
-		bool interrupt_control();
-		void set_interrupt_control(bool enable);
+		bool alarm2_interrupt_enable()
+		{
+			return extract_bits<1, 1>(registers[0]);
+		}
 
-		bool rate_select1();
-		void set_rate_select1(bool enable);
+		void set_alarm2_interrupt_enable(bool value)
+		{
+			assign_bits<1, 1>(&registers[0], value);
+		}
 
-		bool rate_select2();
-		void set_rate_select2(bool enable);
+		bool interrupt_control()
+		{
+			return extract_bits<2, 1>(registers[0]);
+		}
 
-		bool convert_temperature();
-		void set_convert_temperature(bool enable);
+		void set_interrupt_control(bool value)
+		{
+			assign_bits<2, 1>(&registers[0], value);
+		}
 
-		bool battery_backed_square_wave_enable();
-		void set_battery_backed_square_wave_enable(bool enable);
+		bool rate_select1()
+		{
+			return extract_bits<3, 1>(registers[0]);
+		}
 
-		bool disable_oscillator();
-		void set_disable_oscillator(bool enable);
+		void set_rate_select1(bool value)
+		{
+			assign_bits<3, 1>(&registers[0], value);
+		}
 
-		bool retrieve();
-		void store();
+		bool rate_select2()
+		{
+			return extract_bits<4, 1>(registers[0]);
+		}
+
+		void set_rate_select2(bool value)
+		{
+			assign_bits<4, 1>(&registers[0], value);
+		}
+
+		bool convert_temperature()
+		{
+			return extract_bits<5, 1>(registers[0]);
+		}
+
+		void set_convert_temperature(bool value)
+		{
+			assign_bits<5, 1>(&registers[0], value);
+		}
+
+		bool battery_backed_square_wave_enable()
+		{
+			return extract_bits<6, 1>(registers[0]);
+		}
+
+		void set_battery_backed_square_wave_enable(bool value)
+		{
+			assign_bits<6, 1>(&registers[0], value);
+		}
+
+		bool disable_oscillator()
+		{
+			return extract_bits<7, 1>(registers[0]);
+		}
+
+		void set_disable_oscillator(bool value)
+		{
+			assign_bits<7, 1>(&registers[0], value);
+		}
+
+		bool retrieve()
+		{
+			const uint8_t address = 0x0e;
+
+			i2c->blocking_write(0x68, &address, sizeof(address), true);
+			return i2c->blocking_read(0x68, registers, sizeof(registers));
+		}
+
+		void store()
+		{
+			uint8_t buffer[sizeof(registers) + 1] = { 0x0e };
+			std::copy(std::begin(registers), std::end(registers), &buffer[1]);
+
+			i2c->blocking_write(0x68, buffer, sizeof(buffer), true);
+		}
 
 	private:
-		volatile T* i2c;
+		T* i2c;
 		uint8_t registers[1];
 	};
-
-	template <I2c T>
-	Control<T>::Control(volatile T* i2c)
-		: i2c(i2c), registers{}
-	{
-	}
-
-	template <I2c T>
-	bool Control<T>::alarm1_interrupt_enable()
-	{
-		return extract_bits(registers, sizeof(registers), 0x00, 0x0, 0x1);
-	}
-
-	template <I2c T>
-	void Control<T>::set_alarm1_interrupt_enable(bool enable)
-	{
-		assign_bits(registers, sizeof(registers), 0x00, 0x0, 0x1, enable ? 1 : 0);
-	}
-
-	template <I2c T>
-	bool Control<T>::alarm2_interrupt_enable()
-	{
-		return extract_bits(registers, sizeof(registers), 0x00, 0x1, 0x1);
-	}
-
-	template <I2c T>
-	void Control<T>::set_alarm2_interrupt_enable(bool enable)
-	{
-		assign_bits(registers, sizeof(registers), 0x00, 0x1, 0x1, enable ? 1 : 0);
-	}
-
-	template <I2c T>
-	bool Control<T>::interrupt_control()
-	{
-		return extract_bits(registers, sizeof(registers), 0x00, 0x2, 0x1);
-	}
-
-	template <I2c T>
-	void Control<T>::set_interrupt_control(bool enable)
-	{
-		assign_bits(registers, sizeof(registers), 0x00, 0x2, 0x1, enable ? 1 : 0);
-	}
-
-	template <I2c T>
-	bool Control<T>::rate_select1()
-	{
-		return extract_bits(registers, sizeof(registers), 0x00, 0x3, 0x1);
-	}
-
-	template <I2c T>
-	void Control<T>::set_rate_select1(bool enable)
-	{
-		assign_bits(registers, sizeof(registers), 0x00, 0x3, 0x1, enable ? 1 : 0);
-	}
-
-	template <I2c T>
-	bool Control<T>::rate_select2()
-	{
-		return extract_bits(registers, sizeof(registers), 0x00, 0x4, 0x1);
-	}
-
-	template <I2c T>
-	void Control<T>::set_rate_select2(bool enable)
-	{
-		assign_bits(registers, sizeof(registers), 0x00, 0x4, 0x1, enable ? 1 : 0);
-	}
-
-	template <I2c T>
-	bool Control<T>::convert_temperature()
-	{
-		return extract_bits(registers, sizeof(registers), 0x00, 0x5, 0x1);
-	}
-
-	template <I2c T>
-	void Control<T>::set_convert_temperature(bool enable)
-	{
-		assign_bits(registers, sizeof(registers), 0x00, 0x5, 0x1, enable ? 1 : 0);
-	}
-
-	template <I2c T>
-	bool Control<T>::battery_backed_square_wave_enable()
-	{
-		return extract_bits(registers, sizeof(registers), 0x00, 0x6, 0x1);
-	}
-
-	template <I2c T>
-	void Control<T>::set_battery_backed_square_wave_enable(bool enable)
-	{
-		assign_bits(registers, sizeof(registers), 0x00, 0x6, 0x1, enable ? 1 : 0);
-	}
-
-	template <I2c T>
-	bool Control<T>::disable_oscillator()
-	{
-		return extract_bits(registers, sizeof(registers), 0x00, 0x7, 0x1);
-	}
-
-	template <I2c T>
-	void Control<T>::set_disable_oscillator(bool enable)
-	{
-		assign_bits(registers, sizeof(registers), 0x00, 0x7, 0x1, enable ? 1 : 0);
-	}
-
-	template <I2c T>
-	bool Control<T>::retrieve()
-	{
-		static const uint8_t address = 0x0E;
-
-		i2c->blocking_write(0x68, &address, sizeof(address), true);
-		return i2c->blocking_read(0x68, registers, sizeof(registers));
-	}
-
-	template <I2c T>
-	void Control<T>::store()
-	{
-		static const uint8_t address = 0x0E;
-
-		uint8_t writeBuffer[sizeof(registers) + 1];
-		writeBuffer[0] = address;
-		writeBuffer[1] = registers[0];
-		i2c->blocking_write(0x68, writeBuffer, sizeof(writeBuffer), true);
-	}
 }
